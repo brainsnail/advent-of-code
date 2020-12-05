7389b8244ffebae4eb318503eea8a9c521e24b46
logan <444193+brainsnail@users.noreply.github.com>
2020-12-05 13:29:53 -0800

    Day 2 - part 2 - Go

--- 2020/go/02.go
+++ 2020/go/02.go
@@ -11,35 +11,49 @@ var inputFile = "../inputs/02.txt"
 
 func main() {
 
-	var validpasswords int
+	var oldPasswordPolicyMatches, corporatePasswordPolicyMatches int
 
 	f, err := os.Open(inputFile)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	// 9-12 q: qqqxhnhdmqqqqjz
-
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		line := scanner.Text()
-		var min, max int
+		var first, second int
 		var letter, password string
-		_, err := fmt.Sscanf(line, "%d-%d %1s: %s\n", &min, &max, &letter, &password)
+		_, err := fmt.Sscanf(line, "%d-%d %1s: %s\n", &first, &second, &letter, &password)
 		if err != nil {
 			fmt.Println(err)
 		}
-		if validpassword(min, max, letter, password) {
-			validpasswords++
+		if matchesSledPlaceDownTheRoad(first, second, letter, password) {
+			oldPasswordPolicyMatches++
+		}
+		if matchesNewCorporatePolicy(first, second, letter, password) {
+			corporatePasswordPolicyMatches++
 		}
 	}
-	fmt.Println(validpasswords)
+	fmt.Printf("Old Company Passwords: %d \n", oldPasswordPolicyMatches)
+	fmt.Printf("Corporate Company Passwords: %d \n", corporatePasswordPolicyMatches)
 }
 
-func validpassword(min, max int, letter, password string) bool {
+func matchesSledPlaceDownTheRoad(min, max int, letter, password string) bool {
 	charactercount := strings.Count(password, letter)
 	if charactercount < min || charactercount > max {
 		return false
 	}
 	return true
+}
+
+func matchesNewCorporatePolicy(firstposition, secondposition int, letter, password string) bool {
+	totalcount := 0
+	if string(password[firstposition-1]) == letter {
+		totalcount++
+	}
+
+	if string(password[secondposition-1]) == letter {
+		totalcount++
+	}
+	return totalcount == 1
 }
